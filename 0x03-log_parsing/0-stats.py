899b40c06a9b2ba192145d5a99b1f99ab036064a
Sammie2022 <samuelnganganganga26@gmail.com>
2023-07-18 14:28:11 +0300

    log metrics calculator

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,54 @@
+#!/usr/bin/python3
+''' Write a script that reads stdin line by line and computes metrics
+    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" 
+    <status code> <file size>
+    After every 10 lines and/or a keyboard interruption 
+    (CTRL + C), print these statistics from the beginning
+'''
+
+
 import sys
-import signal
 
-# Initialize variables
-lines = []
+'''store the count in a dictionary'''
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
+
 total_size = 0
-status_codes = {}
+count = 0  # keep count of the number lines counted
 
-# Signal handler for keyboard interruption (CTRL + C)
-def signal_handler(sig, frame):
-    print_statistics()
-    sys.exit(0)
-
-# Function to print the statistics
-def print_statistics():
-    print(f"Total file size: {total_size}")
-    for code in sorted(status_codes):
-        print(f"{code}: {status_codes[code]}")
-
-# Register the signal handler for keyboard interruption (CTRL + C)
-signal.signal(signal.SIGINT, signal_handler)
-
-# Read lines from stdin and compute metrics
 try:
     for line in sys.stdin:
-        line = line.strip()
-        parts = line.split()
+        line_list = line.split(" ")
 
-        # Check if line matches the expected format
-        if len(parts) != 7 or parts[2] != 'GET' or not parts[3].startswith('/projects/') or parts[4] not in ('200', '301', '400', '401', '403', '404', '405', '500'):
-            continue
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
 
-        # Extract relevant information from the line
-        file_size = int(parts[6])
-        status_code = parts[4]
+            # check if the status code receive exists in the dictionary and
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
 
-        # Update the metrics
-        lines.append(line)
-        total_size += file_size
-        status_codes[status_code] = status_codes.get(status_code, 0) + 1
+            # update total size
+            total_size += file_size
 
-        # Print statistics after every 10 lines
-        if len(lines) % 10 == 0:
-            print_statistics()
+            # update count of lines
+            count += 1
 
-except KeyboardInterrupt:
-    # Handle keyboard interruption (CTRL + C)
-    print_statistics()
-    sys.exit(0)
+        if count == 10:
+            count = 0  # reset count
+            print('File size: {}'.format(total_size))
 
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
